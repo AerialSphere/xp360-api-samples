8ae6f760149e043e10c4821b57cbe55f18a45fb8
Ben Clifford <ben.w.clifford@gmail.com>
2021-01-21 00:43:36 -0700

    Create sample.js

---
+++ new AerialSphere/sample.js
@@ -0,0 +1,16 @@
+const sphereData: SphereData = {
+  sphereLat: 33.323741448909225,
+  sphereLng: -111.73930440609989,
+  title: 'Example',
+  lookAtLat: 33.32044654603967,
+  lookAtLng: -111.74065486034277,
+  cameraFov: 30,
+  cameraFovMin: 10,
+  cameraFovMax: 145
+}
+
+const aerialSphere: AS = new AerialSphere(
+    'aerialSphereMap',
+    'aerialSphereMapClass',
+    sphereData
+);
