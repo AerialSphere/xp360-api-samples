7397af5617f0674ef210fef066a8889547cf8917
Adam Coyne <adam.coyne@aerialsphere.com>
2021-02-12 19:20:46 -0600

    Update demos

--- 05-set-widget-enabled/demo.js
+++ 05-set-widget-enabled/demo.js
@@ -1,12 +1,6 @@
-const latitude = 36.01070751978051;
-const longitude = -115.17535252989754;
-
 const sphereData = {
-    sphereLat: latitude,
-    sphereLng: longitude,
-    lookAtLat: latitude,
-    lookAtLng: longitude,
-    cameraFov: 30,
+    sphereLat: 36.01070751978051,
+    sphereLng: -115.17535252989754,
 };
 
 const aerialSphere = new AerialSphere(
@@ -15,4 +9,4 @@ const aerialSphere = new AerialSphere(
     sphereData
 );
 
-aerialSphere.addMarker(latitude, longitude, undefined, 'Test Marker');
+aerialSphere.setWidgetEnabled(["fullScreen", "navigation"], false);
