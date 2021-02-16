6d5c7881685cb81ec5b6b829583e22b107d3ef71
radamejr <robert.adame@aerialsphere.com>
2021-02-16 16:11:47 -0700

    added OpenPanoById demo

---
+++ 07-open-pano-by-id/demo.js
@@ -0,0 +1,24 @@
+const sphereData = {
+    sphereLat: 37.8199286,
+    sphereLng: -122.4782551,
+    lookAtLat: 37.815,
+    lookAtLng: -122.4775,
+    cameraFov: 90,
+    cameraFovMin: 10,
+    cameraFovMax: 145,
+    layers: []
+};
+
+const aerialSphere = new AerialSphere(
+    'aerialSphereDivId',
+    'aerialSphereIframeClassname',
+    sphereData
+);
+
+const hideMarkers = () => {
+    aerialSphere.setLayerVisibility('default', false)
+}
+
+const showMarkers = () => {
+    aerialSphere.setLayerVisibility('default', true)
+}
