4d637d43c1afc9cc78c37b44c74c2bab073f35da
radamejr <robert.adame@aerialsphere.com>
2021-02-17 22:04:00 -0700

    fix look at data

--- 11-look-at/demo.js
+++ 11-look-at/demo.js
@@ -1,8 +1,6 @@
 const sphereData = {
     sphereLat: 37.8199286,
     sphereLng: -122.4782551,
-    lookAtLat: 37.815,
-    lookAtLng: -122.4775,
     cameraFov: 90,
     cameraFovMin: 10,
     cameraFovMax: 145,
@@ -16,16 +14,16 @@ const aerialSphere = new AerialSphere(
 );
 
 const lookUp = () => {
-    aerialSphere.lookAt(0, 90)
-}
-
-const lookDown = () => {
     aerialSphere.lookAt(0, -90)
 }
 
+const lookDown = () => {
+    aerialSphere.lookAt(0, 180)
+}
+
 const lookLeft = () => {
-    aerialSphere.lookAt(-90, 0)
+    aerialSphere.lookAt(180, 90)
 }
 const lookRight = () => {
-    aerialSphere.lookAt(90, 0)
+    aerialSphere.lookAt(-90, 90)
 }
