aacdc0901cecb0002ddc5d3c1fa4a77cd3fae487
Trupti Khatavkar <khatavkar.trupti@gmail.com>
2021-08-23 13:10:18 -0700

    updates to setCustomMarkerDistance and setCustomMarkerList to make the fiddle look good

--- 30-set-custom-marker-distance/demo.js
+++ 30-set-custom-marker-distance/demo.js
@@ -1,17 +1,36 @@
 const sphereData = {
     sphereLat: 33.323741448909225,
     sphereLng: -111.73930440609989,
-    lookAtLat: 37.815,
-    lookAtLng: -122.4775,
-    cameraFov: 90,
+    lookAtLat: 33.32044654603967,
+    lookAtLng: -111.74065486034277,
+    cameraFov: 30,
     cameraFovMin: 10,
-    cameraFovMax: 360,
+    cameraFovMax: 145,
     panoMarkerDistance: 10000,
     layers: [
         {
             name: 'default',
             visible: true,
             markers: [
+                {
+                    id: 200,
+                    lat: 33.32014536578641,
+                    lng: -111.74096326093542,
+                    name: '<p>Nurburgring</p>',
+                    icon: 'finish',
+                },
+                {
+                    id: 201,
+                    lat: 33.32046494747296,
+                    lng: -111.74111692682645,
+                    name:
+                        '<h2>Letter C</h2><a href="https://topgolf.com/us/gilbert/">view website</a>',
+                    description:
+                        '<p>THE ONLY PROVIDER OF GEORECTIFIED, 360-DEGREE AERIAL PANORAMAS</p><p>AerialSphere is the only company to combine 360-degree aerial imagery with location data, giving companies new vantage points to make better informed decisions. Built upon nearly a decade of research and patented technological innovation, AerialSphere is changing how companies plan, develop and market their properties, products, and services. Our dedication to innovation has positioned us as a driving market force in a number of industries including real estate, retail, insurance and entertainment.</p>',
+                    icon: 'letter_c',
+                    hoverText: 'hover',
+                    popup: false,
+                },
                 {
                     id: 501,
                     name: '<p>Main Event Gilbert</p>',
--- 31-set-custom-marker-list/demo.js
+++ 31-set-custom-marker-list/demo.js
@@ -1,11 +1,11 @@
 const sphereData = {
     sphereLat: 33.323741448909225,
     sphereLng: -111.73930440609989,
-    lookAtLat: 33.32157801905037,
-    lookAtLng: -111.73772630717852,
-    cameraFov: 165,
+    lookAtLat: 33.32044654603967,
+    lookAtLng: -111.74065486034277,
+    cameraFov: 30,
     cameraFovMin: 10,
-    cameraFovMax: 180,
+    cameraFovMax: 145,
     panoMarkerDistance: 10000,
     panoMarkerList: [],
     layers: [
@@ -13,6 +13,25 @@ const sphereData = {
             name: 'default',
             visible: true,
             markers: [
+                {
+                    id: 200,
+                    lat: 33.32014536578641,
+                    lng: -111.74096326093542,
+                    name: '<p>Nurburgring</p>',
+                    icon: 'finish',
+                },
+                {
+                    id: 201,
+                    lat: 33.32046494747296,
+                    lng: -111.74111692682645,
+                    name:
+                        '<h2>Letter C</h2><a href="https://topgolf.com/us/gilbert/">view website</a>',
+                    description:
+                        '<p>THE ONLY PROVIDER OF GEORECTIFIED, 360-DEGREE AERIAL PANORAMAS</p><p>AerialSphere is the only company to combine 360-degree aerial imagery with location data, giving companies new vantage points to make better informed decisions. Built upon nearly a decade of research and patented technological innovation, AerialSphere is changing how companies plan, develop and market their properties, products, and services. Our dedication to innovation has positioned us as a driving market force in a number of industries including real estate, retail, insurance and entertainment.</p>',
+                    icon: 'letter_c',
+                    hoverText: 'hover',
+                    popup: false,
+                },
                 {
                     id: 501,
                     name: '<p>Main Event Gilbert</p>',
