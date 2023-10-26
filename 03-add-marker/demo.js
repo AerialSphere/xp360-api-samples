a40ea2938975c1e64b818e74708218272a7b29b0
Trupti Khatavkar <trupti.khatavkar@aerialsphere.com>
2023-10-26 11:24:25 -0700

    updated addMarker to take in an object

--- 03-add-marker/demo.js
+++ 03-add-marker/demo.js
@@ -16,49 +16,49 @@ const aerialSphere = new AerialSphere(
 );
 
 /* Basic marker */
-aerialSphere.addMarker(baseLatitude, baseLongitude, undefined, "Test Marker 1");
+aerialSphere.addMarker({
+    lat: baseLatitude,
+    lng: baseLongitude, 
+    name: "Test Marker 1"
+});
 
 /* Custom marker */
-aerialSphere.addMarker(
-  baseLatitude + 0.0006,
-  baseLongitude + 0.0006,
-  undefined, // address
-  "Test Marker 2", // name/title
-  undefined, // description
-  { // custom icon
-    url: "https://c2.peakpx.com/wallpaper/1003/267/588/hand-yes-ok-isolated-background-wallpaper-preview.jpg",
-    teardrop: true,
-    height: 40, 
-    width: 40
+aerialSphere.addMarker({
+    lat: baseLatitude + 0.0006,
+    lng: baseLongitude + 0.0006,
+    name: "Test Marker 2", // name/title
+    icon: { // custom icon
+        url: "https://c2.peakpx.com/wallpaper/1003/267/588/hand-yes-ok-isolated-background-wallpaper-preview.jpg",
+        teardrop: true,
+        height: 40, 
+        width: 40
+    }
 });
 
 /* Marker with fancy description */
-aerialSphere.addMarker(
-  baseLatitude + 0.0012,
-  baseLongitude + 0.0012,
-  undefined, // address
-  "Test Marker 3", // name/title
-  `<span style="color:blue">I am blue</span>
-  <br>
-  <b>I am bold</b>
-  <hr>
-  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Thumbs_up_icon.svg/1877px-Thumbs_up_icon.svg.png" alt="big thumbs up" width="90" height="90">
-  `// description
-);
+aerialSphere.addMarker({
+    lat: baseLatitude + 0.0012,
+    lng: baseLongitude + 0.0012,
+    name: "Test Marker 3", // name/title
+    description: `<span style="color:blue">I am blue</span>
+        <br>
+        <b>I am bold</b>
+        <hr>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Thumbs_up_icon.svg/1877px-Thumbs_up_icon.svg.png" alt="big thumbs up" width="90" height="90">`// description
+});
 
 /* Marker wih some of everything */
-aerialSphere.addMarker(
-  baseLatitude + 0.0018,
-  baseLongitude + 0.0018,
-  undefined, // address
-  "Test Marker 4", // name/title
-  "I have metadata", // description
-  "park", // icon
-  "parks-in-the-area", // layer
-  "user-selected-id", // id
-  "I show on hover", // hoverText
-  true, // popup
-  false, // isStatic
-  {"item 1": "example metadata"}, // metaData
-  "#00A19C", // iconColor
-);
+aerialSphere.addMarker({
+    lat: baseLatitude + 0.0018,
+    lng: baseLongitude + 0.0018,
+    name: "Test Marker 4", // name/title
+    description: "I have metadata", // description
+    icon: "park", // icon
+    layer: "parks-in-the-area", // layer
+    id: "user-selected-id", // id
+    hoverText: "I show on hover", // hoverText
+    popup: true, // popup
+    isStatic: false, // isStatic
+    metaData: {"item 1": "example metadata"}, // metaData
+    iconColor: "#00A19C", // iconColor
+});
